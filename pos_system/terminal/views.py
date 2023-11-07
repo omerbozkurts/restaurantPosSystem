e34462fea0f0d1cbd735d88aabaa862588b47bd9
Burakherdogan <94480490+Burakherdogan@users.noreply.github.com>
2023-11-07 19:03:58 +0300

    Delete pos_system directory

--- pos_system/terminal/views.py
+++
@@ -1,9 +0,0 @@
-from django.shortcuts import render
-from django.http import HttpResponse
-
-
-def index_view(request):
-    data = {
-        "tables": range(10)
-    }
-    return render(request, "index.html",data)
